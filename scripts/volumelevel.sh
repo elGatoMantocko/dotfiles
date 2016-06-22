58caaf1bd52107abfc45b9cc11b9203defc048c4
Elliott Mantock <emantock@gmail.com>
2016-06-21 22:36:37 -0400

    trying to reduce cpu consumption

--- scripts/volumelevel.sh
+++ scripts/volumelevel.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 2 )) | tail -n 1| sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
+sinks=$(pactl list sinks)
+mute=$(echo "$sinks" | grep '^[[:space:]]Mute:' | head -n $(( $SINK + 2)) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' | awk '{print $2}')
+vol=$(echo "$sinks" | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 2 )) | tail -n 1| sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+if [ $mute == "yes" ]; then
+  echo "[$vol]"
+else
+  echo $vol
+fi
+
