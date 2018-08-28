f83e64b59b431cf2dafc55853e484601a60905a1
Elliott Mantock <emantock@egencia.com>
2018-08-28 07:25:06 -0700

    added install step for mac (installs homebrew vim and git)

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env zsh
+# install homebrew on mac if it isn't already installed
+[[ "$(uname -s)" == "Darwin" ]] && [[ "$(which brew)" == "brew not found" ]] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+[[ "$(uname -s)" == "Darwin" ]] && brew update && brew install vim git
+
 for dir (*/); do
   ins_script=$(echo $(dirname $0:a)/$dir | sed 's/\/$/\/install.sh/g');
   [ -x $ins_script ]; echo "Installing $dir" && zsh $ins_script;
